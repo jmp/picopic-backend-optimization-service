59e7f2844d38221c101e9c6369713f68727bf706
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-09-08 22:16:31 +0300

    Remove extra logging

--- functions/create_download_url/index.py
+++ functions/create_download_url/index.py
@@ -18,23 +18,15 @@ ALLOWED_FILE_TYPES = {"image/png": "png"}
 def handler(event, context):
     bucket = environ["BUCKET"]
     key = event["pathParameters"]["key"]
-    logger.info("Getting object %s from bucket %s.", key, bucket)
     original_object = s3_client.get_object(Bucket=bucket, Key=key)
-    logger.info("Reading object body.")
     body = original_object["Body"].read()
-    logger.info("Verifying image type.")
     mime = _guess_mime_type(body)
     if mime not in ALLOWED_FILE_TYPES.keys():
-        logger.warning("Attempted to optimize invalid file type %s.", mime)
-        logger.info("Deleting invalid object %s...", key)
         s3_client.delete_object(Bucket=bucket, Key=key)
-        logger.info("Invalid object %s deleted.", key)
         return {
             "statusCode": 400,
             "body": dumps({"message": "File type not allowed."}),
         }
-    logger.info("File type detected as %s.", mime)
-    logger.info("Optimizing %s bytes...", len(body))
     optimized_body = _optimize(body)
     if optimized_body is None:
         s3_client.delete_object(Bucket=bucket, Key=key)
@@ -42,10 +34,7 @@ def handler(event, context):
             "statusCode": 400,
             "body": dumps({"message": "Image could not be optimized."}),
         }
-    logger.info("Optimized to %s bytes.", len(optimized_body))
-    logger.info("Putting optimized object %s to bucket %s.", key, bucket)
     s3_client.put_object(Bucket=bucket, Key=key, Body=optimized_body)
-    logger.info("Generating presigned URL.")
     filename = f"optimized.{ALLOWED_FILE_TYPES[mime]}"
     url = s3_client.generate_presigned_url(
         "get_object",
@@ -56,7 +45,6 @@ def handler(event, context):
         },
         ExpiresIn=10,
     )
-    logger.info("Presigned URL created; returning successfully.")
     return {"statusCode": 200, "body": dumps({"url": url})}
 
 
@@ -70,9 +58,8 @@ def _optimize(data: bytes) -> Optional[bytes]:
     try:
         optimized_data = ZopfliPNG().optimize(data)
         if optimized_data[:8] != PNG_HEADER:
-            logger.error("Invalid optimized image: %s", optimized_data)
             raise ValueError("Corrupted PNG generated.")
         return optimized_data
     except ValueError:
         logger.exception("Failed to optimize image.")
-    return None
+        return None
